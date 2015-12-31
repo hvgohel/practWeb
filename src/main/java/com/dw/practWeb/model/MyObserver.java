6f9c0eec0b1ecc52038b4c33b82d54601b66d9ec
hiren gohel <hirengohel@dreamworldsolutions.com>
2015-12-31 14:19:23 +0530

    reactive

---
+++ src/main/java/com/dw/practWeb/model/MyObserver.java
@@ -0,0 +1,32 @@
+package com.dw.practWeb.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import rx.Observer;
+
+public class MyObserver implements Observer<Student>
+{
+    private static Logger logger = LoggerFactory.getLogger(MyObserver.class);
+
+    @Override
+    public void onCompleted()
+    {
+        logger.debug("onCompleted() :: all student created successfully");
+    }
+
+    @Override
+    public void onError(Throwable e)
+    {
+        logger.debug("onError() :: {} ", e);
+    }
+
+    @Override
+    public void onNext(Student student)
+    {
+        //BeanUtils.only().getStudentService().add(student);
+
+        logger.debug("onNext() :: student {} created successfully", student.getId());
+    }
+
+}
---
+++ src/main/java/com/dw/practWeb/model/MyRxJavaSchedulersHook.java
@@ -0,0 +1,26 @@
+package com.dw.practWeb.model;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import rx.Scheduler;
+import rx.plugins.RxJavaSchedulersHook;
+import rx.schedulers.Schedulers;
+
+public class MyRxJavaSchedulersHook extends RxJavaSchedulersHook
+{
+    ExecutorService executor;
+
+    @Override
+    public Scheduler getComputationScheduler()
+    {
+        System.out.println("getComputationScheduler invoked");
+        executor = Executors.newFixedThreadPool(10);
+        return Schedulers.from(executor);
+    }
+
+    public void shutDown()
+    {
+        executor.shutdown();
+    }
+}
