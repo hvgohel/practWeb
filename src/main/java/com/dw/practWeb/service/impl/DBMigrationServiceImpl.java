7d58045093c40bb10b75b190db2d3e157a4b4bca
Ajay dhameliya <ajay@dreamworldsolutions.com>
2016-01-21 11:53:20 +0530

    added DBMigrationService

---
+++ src/main/java/com/dw/practWeb/service/impl/DBMigrationServiceImpl.java
@@ -0,0 +1,14 @@
+package com.dw.practWeb.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dw.practWeb.service.DBMigrationService;
+
+@Service
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+public class DBMigrationServiceImpl implements DBMigrationService
+{
+
+}
